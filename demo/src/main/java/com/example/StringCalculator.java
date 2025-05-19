3e35d63115907d251e7f68e17fbf6669a9c0a14d
Radadzi4038 <radadzi4038@conestogac.on.ca>
2025-05-19 00:37:04 -0400

    Changed return digit back to 0

--- demo/src/main/java/com/example/StringCalculator.java
+++ demo/src/main/java/com/example/StringCalculator.java
@@ -20,7 +20,7 @@ public class StringCalculator {
 
     public int add(String numbers) {
         if (numbers.isEmpty()) {
-            return 2;
+            return 0;
         }
         String[] numArray = numbers.split(",");
         if (numArray.length == 1) {
